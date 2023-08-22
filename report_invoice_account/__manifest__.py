d7eff445b2ad4017ac7f80d80997ce741112cacf
Elaraby95 <132716221+Elaraby95@users.noreply.github.com>
2023-08-22 19:34:17 +0300

    Add files via upload
    edit report size

---
+++ report_invoice_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "REPORT INVOICE",
+    "summary": "Modify Invoice Account",
+    "depends": ["account", "product"],
+    "data": [
+        "reports/invoice_report.xml",
+        "reports/inovice_main.xml",
+    ],
+    'version': '1.0',
+    'installable': True,
+    'license': 'LGPL-3',
+}
